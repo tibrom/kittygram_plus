7915811ead32b7540ccf0aaf0efbf24362921cc2
Андрей Горланов <gorlans@mail.ru>
2022-11-01 13:05:24 +0300

    pass

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,22 +1,45 @@
+import datetime as dt
+
+import webcolors
 from rest_framework import serializers
 
 from .models import Cat, Owner, Achievement,  AchievementCat
 
 
+CHOICES = (
+        ('Gray', 'Серый'),
+        ('Black', 'Чёрный'),
+        ('White', 'Белый'),
+        ('Ginger', 'Рыжий'),
+        ('Mixed', 'Смешанный'),
+    )
+
 class AchievementSerializer(serializers.ModelSerializer):
 
+    achievement_name = serializers.CharField(source='name')
     class Meta:
         model = Achievement
-        fields = ('id', 'name') 
+        fields = ('id', 'achievement_name') 
+
+
+class CatListSerializer(serializers.ModelSerializer):
+    color = serializers.ChoiceField(choices=CHOICES)
+    
+    class Meta:
+        model = Cat
+        fields = ('id', 'name', 'color') 
 
 
 class CatSerializer(serializers.ModelSerializer):
     
     achievements = AchievementSerializer(required=False, many=True)
+    age = serializers.SerializerMethodField()
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements', 'age')
 
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
         
     
     def create(self, validated_data):
@@ -43,4 +66,6 @@ class OwnerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Owner
         fields = ('first_name', 'last_name', 'cats')
+
+
 
